d340ad620b5b63e233f2b7ff44bd0900161abd78
RajputYash <yashrajput8130@gmail.com>
2017-09-03 14:14:20 +0530

    spy name has been added

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print "hello python"
+print "let\'s get started "
+
+spy_name = raw_input("what is your name :")
+
+spy_salutation = raw_input("what should we call you :")
+
+spy_name = spy_salutation + " " + spy_name
+
+print "Welcome" + " " + spy_name + " " + "glad to have u "
